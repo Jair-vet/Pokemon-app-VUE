52f40598cdfdee05a56d58698293d70533ab7e8d
Jair <jairaceves56@gmail.com>
2021-12-19 09:35:17 -0600

    Mostramos al pokemon que tiene el id

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -5,9 +5,9 @@
         class="hidden-pokemon"
         alt="pokemon">
 
-  <!-- <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" 
+  <img :src="imgSrc" 
         class="fade-in"
-        alt="pokemon"> -->
+        alt="pokemon">
 
 </div>
 </template>
