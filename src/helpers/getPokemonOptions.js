41a862ca55745c839774d614135d12e513c370cc
Jair <jairaceves56@gmail.com>
2021-12-19 10:24:59 -0600

    Creamos los Arreglos y los mandamos a hablar

--- src/helpers/getPokemonOptions.js
+++ src/helpers/getPokemonOptions.js
@@ -1,3 +1,4 @@
+import { Promise } from "core-js"
 import pokemonApi from "../api/pokemonApi"
 
 const getPokemons = () => {
@@ -9,12 +10,15 @@ const getPokemons = () => {
 }
 
 
-const getPokemonOptions = () => {
+const getPokemonOptions = async() => {
     
     const mixPokemons = getPokemons()
                             .sort( () => Math.random() -0.5 )
 
-    getPokemonsNames( mixPokemons.splice(0,4) ) // Corta y toma los primero 4 
+    const pokemons = await getPokemonsNames( mixPokemons.splice(0,4) ) // Corta y toma los primero 4 
+    
+    return pokemons
+
 }
 
 // const getPokemonsNames = ( pokemons = [] ) => { // Unicamnete debo recibir un arreglo
@@ -23,8 +27,22 @@ const getPokemonsNames = async( [a,b,c,d] = [] ) => { // Desestructuramos el are
         // Forma de extraer la data
     // const resp = await pokemonApi.get(`/1`)  
     // console.log(resp.data.name, resp.data.id);
-
     // console.log(a,b,c,d);
+    const primiseArr = [
+        pokemonApi.get(`/${ a }`),
+        pokemonApi.get(`/${ b }`),
+        pokemonApi.get(`/${ c }`),
+        pokemonApi.get(`/${ d }`),
+    ]
+
+    const [ p1, p2, p3, p4 ] = await Promise.all( primiseArr )
+
+    return [
+        { name: p1.data.name, id: p1.data.id},
+        { name: p2.data.name, id: p2.data.id},
+        { name: p3.data.name, id: p3.data.id},
+        { name: p4.data.name, id: p4.data.id},
+    ]
 
 }
 
